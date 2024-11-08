018322074ab6ba70435070b61fdd46482ee04649
A-Shalchian <162397874+A-Shalchian@users.noreply.github.com>
2024-11-08 14:43:35 -0500

    added catering page

---
+++ src/app/catering/page.tsx
@@ -0,0 +1,11 @@
+// src/app/catering/page.tsx
+import React from "react";
+
+const CateringPage = () => {
+  return (
+    <div>
+      <h1>Catering page</h1>
+    </div>
+  );
+};
+export default CateringPage;
