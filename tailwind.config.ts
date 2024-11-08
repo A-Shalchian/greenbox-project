051873da5ba3481dce47dff136cb044d1e3f1a95
A-Shalchian <162397874+A-Shalchian@users.noreply.github.com>
2024-11-08 01:10:02 -0500

    initial commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,32 @@
 import type { Config } from "tailwindcss";
 
-export default {
+const config: Config = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    screens: {
+      sm: "375px",
+      md: "768px",
+      lg: "1200px",
+      xl: "1440px",
+    },
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: "1rem",
+        md: "2rem",
+      },
+    },
     extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+      fontFamily: {
+        sans: "var(--font-sans)",
+        serif: "var(--font-serif)",
       },
     },
   },
   plugins: [],
-} satisfies Config;
+};
+export default config;
